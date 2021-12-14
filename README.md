13ddbcbf0fa573ba23db6cc157f6a114af6de1d7
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2021-12-14 13:00:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+SNA-Graph-Recommendation
+
+# Dataset
+
+The dataset of our project is MovieLens. For developing we will use a subsampling of the dataset. The link can be found [here](https://github.com/srviest/movie-recommender/tree/master/dataset/movielens/small). The dataset for production can be found [here](https://grouplens.org/datasets/movielens/).
+
+Note: The development dataset is preloaded in the project. You do not need to download.
---
+++ main.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import networkx as nx
+
+
+class Dataset:
+    def __init__(self, directory):
+        print('Loading local database...')
+        self.links = pd.read_csv(directory+'links.csv', sep=',', encoding='utf-8')
+        self.movies = pd.read_csv(directory+'movies.csv', sep=',', encoding='utf-8')
+        self.ratings = pd.read_csv(directory+'ratings.csv', sep=',', encoding='utf-8')
+        self.tags = pd.read_csv(directory+'tags.csv', sep=',', encoding='utf-8')
+        print('Database loaded')
+
+    def getLinks(self):
+        return self.links
+
+    def getMovies(self):
+        return self.movies
+
+    def getRatings(self):
+        return self.ratings
+
+    def getTags(self):
+        return self.tags
+
+
+class Graph:
+    def __init__(self, dataset_directory):
+        self.dataset = Dataset(dataset_directory)
+        self.graph = nx.Graph()
+        self.__initGraph()
+
+    def __initGraph(self):
+        print('Graph initialing...')
+        movies = self.dataset.getMovies()
+        for index, value in self.dataset.getRatings().iterrows():
+            self.graph.add_node(value['userId'], type='user')
+            movie = movies.loc[movies['movieId'] == value['movieId']].iloc[0]
+            self.graph.add_node(movie['title'], type='movie')
+            self.graph.add_edge(value['userId'], movie['title'], rating=value['rating'])
+        print('Graph created')
+        print(f'Bipartite graph: {nx.bipartite.is_bipartite(self.graph)}')
+        print(f'Total nodes: {nx.number_of_nodes(self.graph)}')
+        print(f'Total edges: {nx.number_of_edges(self.graph)}')
+
+
+if __name__ == '__main__':
+    graph = Graph(dataset_directory='data/dataset/')
