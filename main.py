9ac460880861b4a2be230605e81ae4d4bc17fb3e
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2021-12-17 13:16:50 +0200

    svm regression added, test set improved

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import networkx as nx
 import numpy as np
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVR
 
 
 class Dataset:
@@ -41,15 +42,16 @@ class Embedding:
         self.corpus = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime', 'Documentary', 'Drama',
                        'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller',
                        'War', 'Western', '(no genres listed)']
+        self.method = method
         if method == 'tf-idf':
             self.tfidf = TfidfVectorizer()
             self.tfidf.fit(self.corpus)
 
-    def get_tf_idf(self, document):
-        return self.tfidf.transform([document])
-
-    def get_tf(self, document):
-        return [1 if value in document else 0 for value in self.corpus]
+    def transform(self, document):
+        if self.method == 'tf':
+            return [1 if value in document else 0 for value in self.corpus]
+        elif self.method == 'tf-idf':
+            return self.tfidf.transform([document])
 
 
 class Metric:
@@ -73,22 +75,21 @@ class Graph:
 
     def __insert_movies_nodes(self, movies):
         for index, movie in movies.iterrows():
-            self.graph.add_node(movie['title'], type='movie', genres=movie['genres'])
+            self.graph.add_node(movie['title'], type='movie', genres=movie['genres'], movieId=movie['movieId'])
 
-    def __create_movies_graph(self, movies):
-        embedding = Embedding(method='tf')
+    def __create_movies_graph(self, movies, embedding):
         for index_i, movie_i in movies.iterrows():
             index_j = index_i
-            self.graph.add_node(movie_i['title'], type='movie', genres=movie_i['genres'])
+            self.graph.add_node(movie_i['title'], type='movie', genres=movie_i['genres'], movieId=movie_i['movieId'])
             for _, movie_j in movies.iterrows():
                 if index_i != index_j:
-                    self.graph.add_node(movie_j['title'], type='movie', genres=movie_j['genres'])
-                    similarity = Similarity.cosine(embedding.get_tf(movie_i['genres']), embedding.get_tf(movie_j['genres']))
+                    self.graph.add_node(movie_j['title'], type='movie', genres=movie_j['genres'], movieId=movie_j['movieId'])
+                    similarity = Similarity.cosine(embedding.transform(movie_i['genres']), embedding.transform(movie_j['genres']))
                     if similarity > 0.8:
                         self.graph.add_edge(movie_i['title'], movie_j['title'], similarity=similarity)
                 index_j += 1
 
-    def init(self, dataset_directory, keep_only_good_ratings=False, bipartite=False):
+    def init(self, dataset_directory, keep_only_good_ratings=False, bipartite=False, embedding=Embedding('tf')):
         dataset = Dataset(dataset_directory)
         print('Graph initialing...')
         movies = dataset.get_movies()
@@ -96,14 +97,14 @@ class Graph:
         if bipartite:
             self.__insert_movies_nodes(movies.head(500))
         else:
-            self.__create_movies_graph(movies.head(100))
+            self.__create_movies_graph(movies.head(100), embedding)
         for index, value in dataset.get_ratings().iterrows():
             movie = movies.loc[movies['movieId'] == value['movieId']].iloc[0]
             if movie['title'] not in self.graph:
                 continue
             if keep_only_good_ratings and value['rating'] < 3.5:
                 continue
-            self.graph.add_node(value['userId'], type='user')
+            self.graph.add_node(value['userId'], type='user', userId=value['userId'])
             # self.graph.add_node(movie['title'], type='movie', genres=movie['genres'])
             tag = tags.loc[(tags['userId'] == value['userId']) & (tags['movieId'] == value['movieId'])]
             tag_value = '' if tag.empty else tag.iloc[0]['tag']
@@ -140,6 +141,8 @@ class Graph:
                 continue
             if len(test) > test_size:
                 break
+            if edge[2]['rating'] < 3.5:
+                continue
             graph_copy.remove_edge(*edge[:2])
             # print(nx.number_connected_components(graph_copy))
             if nx.number_connected_components(graph_copy) == 1:
@@ -174,6 +177,8 @@ class Heuristic:
             if node[1]['type'] == 'user':
                 edges = self.graph[node[0]]
                 for key in edges:
+                    if edges[key]['rating'] < 3.5:
+                        continue
                     val = edges[key]['rating']/5
                     self.recommended_nodes[node[0]] = {}
                     self.__sim_cal(degree, 1, val, key, node[0])
@@ -196,9 +201,72 @@ class Heuristic:
 
 
 class Learning:
-    def __init__(self):
-        pass
+    def __init__(self, graph, embedding=Embedding('tf')):
+        self.svr = None
+        self.recommended_nodes = None
+        self.graph = graph
+        self.embedding = embedding
+        self.x, self.y, self.movies_title = self.get_x_y(self.graph.edges(data=True))
 
+    def get_x_y(self, edges):
+        x = []
+        y = []
+        movies_title = {}
+        for edge in edges:
+            # print(edge)
+            if 'similarity' in edge[2]:
+                continue
+            if self.graph.nodes[edge[0]]['type'] == 'user':
+                user = self.graph.nodes[edge[0]]
+                movie = self.graph.nodes[edge[1]]
+            else:
+                user = self.graph.nodes[edge[1]]
+                movie = self.graph.nodes[edge[0]]
+            movies_title[movie['movieId']] = movie['label']
+            features = [int(user['userId']), movie['movieId']]
+            for genre in self.embedding.transform(movie['genres']):
+                features.append(genre)
+            # print(features)
+            x.append(features)
+            y.append(edge[2]['rating']/5)
+        return np.array(x), np.array(y), movies_title
+
+    def svm_fit(self):
+        self.svr = SVR()
+        # print(self.x)
+        self.svr.fit(self.x, self.y)
+
+    def svm_predict(self, x, movies_title, top=10):
+        pred = self.svr.predict(x)
+        # print(pred, len(pred))
+        self.__get_predictions(pred, x, movies_title)
+        self.__get_top(top)
+        return self.recommended_nodes
+
+    def __get_predictions(self, predicted, x, movies_title):
+        self.recommended_nodes = {}
+        for i in range(len(x)):
+            # print(x[i][0], x[i][1], movies_title[x[i][1]], predicted[i])
+            user = str(float(x[i][0]))
+            if predicted[i] < 0.7:
+                continue
+            if user not in self.recommended_nodes:
+                self.recommended_nodes[user] = {
+                    movies_title[x[i][1]]: predicted[i]
+                }
+            else:
+                self.recommended_nodes[user][movies_title[x[i][1]]] = predicted[i]
+
+    def __get_top(self, top=10):
+        for user in self.recommended_nodes:
+            top_n = []
+            for i in range(top):
+                if len(self.recommended_nodes[user]) == 0:
+                    break
+                key = max(self.recommended_nodes[user], key=self.recommended_nodes[user].get)
+                top_n.append(key)
+                self.recommended_nodes[user].pop(key)
+            self.recommended_nodes[user] = top_n
 
 class Hybrid:
     def __init__(self):
@@ -212,14 +280,23 @@ class Evaluation:
 
 if __name__ == '__main__':
     graph = Graph()
-    # graph.init(dataset_directory='data/dataset/', keep_only_good_ratings=True, bipartite=False)
+    # graph.init(dataset_directory='data/dataset/', keep_only_good_ratings=False, bipartite=False)
     # graph.export_gexf(directory='data/graph/')
     graph.read_gexf('data/graph/graph.gexf')
     graph_train, test_edges = graph.split_train_test()
     print(len(test_edges))
-    heuristic = Heuristic(graph_train)
-    heuristic.degree_association(4)
-    rec = heuristic.get_recommended_nodes(20)
+    ##### Heuristic #####
+    # heuristic = Heuristic(graph_train)
+    # heuristic.degree_association(4)
+    # rec = heuristic.get_recommended_nodes(20)
+
+    ##### Learning #####
+    learning = Learning(graph_train)
+    learning.svm_fit()
+    embedding = Embedding('tf')
+    test_x, test_y, movies_title = learning.get_x_y(test_edges)
+    rec = learning.svm_predict(np.array(test_x), movies_title, top=20)
+    # print(rec)
     link = []
     for edge in test_edges:
         # print(edge)
@@ -230,10 +307,12 @@ if __name__ == '__main__':
             link.append((edge[1], edge[0], rec[edge[1]]))
             # print(edge[1], edge[0], rec[edge[1]], len(rec[edge[1]]))
     print(len(link))
+
+
     # embedding = Embedding('tf')
-    # test1 = embedding.get_tf('Animation|War|Thriller')
+    # test1 = embedding.transform('Animation|War|Thriller')
     # print(test1)
-    # test2 = embedding.get_tf('War|Thriller')
+    # test2 = embedding.transform('War|Thriller')
     # print(test2)
     # sim = Similarity.cosine(test1, test2)
     # print(sim)
