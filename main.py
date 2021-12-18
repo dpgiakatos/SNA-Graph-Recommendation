924e46cc39f3265effb4187a8e1fae6377b2887a
DimitrisMylonas <72869628+DimitrisMylonas@users.noreply.github.com>
2021-12-18 13:39:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import csrgraph as cg
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVR
+from scipy.spatial import distance
 
 
 class Dataset:
@@ -36,7 +37,21 @@ class Similarity:
     @staticmethod
     def cosine(a, b):
         return np.sqrt(np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b)))
+   
+    @staticmethod
+    def jaccard(a, b):
+        nominator = a.intersection(b)
+        denominator = a.union(b)
+        similarity = len(nominator)/len(denominator)
+        return similarity
 
+    @staticmethod
+    def euclidean_distance(a, b):
+        return distance.euclidean(a, b)
+    
+    @staticmethod
+    def manhattan_distance(a, b):
+        return distance.cityblock(a, b)
 
 class Embedding:
     def __init__(self, method='tf'):
