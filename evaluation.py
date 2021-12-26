b09409a1ed8001d23646bf026dd8577bc6c65b42
DimitrisMylonas <72869628+DimitrisMylonas@users.noreply.github.com>
2021-12-26 18:01:49 +0200

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,8 @@
+from sklearn import metrics
+
 class Evaluation:
-    def __init__(self):
-        pass
+    
+    def model_evaluation(y_test, y_predicted):
+        print("Correlation: %2f, p-value: %2f" % stats.pearsonr(y_test, y_predicted))
+        print("Mean Squared Error: %2f" %  metrics.mean_squared_error(y_test, y_predicted) )
+        print("R-Squared: %2f" % metrics.r2_score(y_test, y_predicted))
